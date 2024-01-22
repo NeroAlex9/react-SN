4aac8c98450c2c3ffcb5d2f19a94d6185285a0b7
NeroAlex9 <Nerobelov1997@mail.ru>
2024-01-22 13:07:50 +0300

    API, side effect, lesson 52

--- src/components/Profile/Users/Users.jsx
+++ src/components/Profile/Users/Users.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import style from "./Users.module.css";
+import axios from "axios";
 
 const Users = (props) => {
-  debugger;
+  const loadUsers = () => {
+    if (props.users.length < 10) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((respons) => {
+          props.setUsers(respons.data.items);
+        });
+    }
+  };
+
   return (
     <div>
+      <button onClick={loadUsers}>Load Users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
-            <img className={style.avatar} src={u.avatar} alt="Ava" />
+            <img
+              className={style.avatar}
+              src={
+                u.photos.small != null
+                  ? u.photos.small && u.photos
+                  : "https://cdn.icon-icons.com/icons2/1390/PNG/512/atom_96224.png"
+              }
+              alt="Ava"
+            />
             <div>
               {u.followed ? (
                 <button
@@ -34,8 +53,8 @@ const Users = (props) => {
             <div>{u.status}</div>
           </span>
           <span>
-            <div>{u.location.country}</div>
-            <div>{u.location.city}</div>
+            <div>{"u.location.country"}</div>
+            <div>{"u.location.city"}</div>
           </span>
         </div>
       ))}
--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -6,32 +6,32 @@ let setUsers = "SET_USERS";
 
 let initialState = {
   users: [
-    {
-      id: 1,
-      name: "Alexey",
-      avatar:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Schematicky_atom.png/683px-Schematicky_atom.png",
-      followed: true,
-      status: "Holder",
-      location: { city: "New-York", country: "USA" },
-    },
-    {
-      id: 2,
-      name: "Maria",
-      avatar:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Stylised_atom_with_three_Bohr_model_orbits_and_stylised_nucleus.svg/904px-Stylised_atom_with_three_Bohr_model_orbits_and_stylised_nucleus.svg.png",
-      followed: true,
-      status: "New holder",
-      location: { city: "Moskow", country: "Russia" },
-    },
-    {
-      id: 3,
-      name: "Sergey",
-      avatar: "https://cdn-icons-png.flaticon.com/512/2784/2784386.png",
-      followed: false,
-      status: "Old holder",
-      location: { city: "Gomel", country: "Belarus" },
-    },
+    // {
+    //   id: 1,
+    //   name: "Alexey",
+    //   photos:
+    //     "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Schematicky_atom.png/683px-Schematicky_atom.png",
+    //   followed: true,
+    //   status: "Holder",
+    //   location: { city: "New-York", country: "USA" },
+    // },
+    // {
+    //   id: 2,
+    //   name: "Maria",
+    //   photos:
+    //     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Stylised_atom_with_three_Bohr_model_orbits_and_stylised_nucleus.svg/904px-Stylised_atom_with_three_Bohr_model_orbits_and_stylised_nucleus.svg.png",
+    //   followed: true,
+    //   status: "New holder",
+    //   location: { city: "Moskow", country: "Russia" },
+    // },
+    // {
+    //   id: 3,
+    //   name: "Sergey",
+    //   photos: "https://cdn-icons-png.flaticon.com/512/2784/2784386.png",
+    //   followed: false,
+    //   status: "Old holder",
+    //   location: { city: "Gomel", country: "Belarus" },
+    // },
   ],
 };
 
@@ -54,7 +54,7 @@ const usersReducer = (state = initialState, action) => {
         ...state,
         users: state.users.map((u) => {
           if (u.id === action.userID) {
-            return { ...u, followed: false};
+            return { ...u, followed: false };
           }
           return u;
         }),
